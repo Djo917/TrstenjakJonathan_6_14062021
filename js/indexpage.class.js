3b948b015a713532be5018af25f92999978218c0
Djo917 <trstenjak.jonathan@gmail.com>
2021-07-20 17:35:40 +0200

    Appel de la methode fetchData de la classe Ajax dans showAllPhotographers et de la méthode renderAllPhotographers

--- js/indexpage.class.js
+++ js/indexpage.class.js
@@ -19,8 +19,8 @@ class IndexPage {
     }
 
     showAllPhotographers() {
-        
-        const datas = this.Ajax.fetchData();
+        const ajax = new Ajax('/data/FishEyeData.json');
+        const datas = ajax.fetchData();
         console.log(datas);
         this.view.renderAllPhotographers(datas);
 
@@ -38,12 +38,11 @@ class IndexPage {
 const indexPage = new IndexPage(new View());
 indexPage.run();
 
-const testAjax = new IndexPage(new Ajax());
-testAjax.run();
 
 
-fetch("data/FishEyeData.json")
-    .then(dataPhotographes => dataPhotographes.json())
-    .then(dataPhotographes => console.log(JSON.stringify(dataPhotographes.photographers[0].name)))  
-    .then(dataPhotographes => console.log(dataPhotographes.photographers[0].name))
-    .catch(error => console.log(error));
+
+// fetch("data/FishEyeData.json")
+//     .then(dataPhotographes => dataPhotographes.json())
+//     .then(dataPhotographes => console.log(JSON.stringify(dataPhotographes.photographers[0].name)))  
+//     .then(dataPhotographes => console.log(dataPhotographes.photographers[0].name))
+//     .catch(error => console.log(error));
