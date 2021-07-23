6a5c2bb91f961ebcf06f0157c3800e739d287bcb
Djo917 <trstenjak.jonathan@gmail.com>
2021-07-23 20:55:34 +0200

    modification du pathing portrait du json, modification du chemin dans img.src de la fonction renderAllPhotographers, redimensionnement des images des photographes pour améliorer le temps de chargement de la page

--- js/view.class.js
+++ js/view.class.js
@@ -6,14 +6,18 @@ export class View {
             const div = document.createElement("div");
             const a = document.createElement("a");
             const img = document.createElement("img");
-            const h2 = document.createElement("h2");  
+            const h2 = document.createElement("h2");
+            const h3 = document.createElement("h3");
+            const p = document.createElement("p");
+            const pbis = document.createElement("p");  
 
             idsection.appendChild(div);
             div.classList.add("photographers__line--profile");
             div.appendChild(a);
+            a.href = "photographer.html"
             a.classList.add("photographers__line--profilelink");
             a.appendChild(img);
-            img.src = photographer.portrait;
+            img.src = `media/sample_photos/Photographers_ID_Photos/${photographer.portrait}`;
             img.classList.add("photographers__line--pictures");
             a.appendChild(h2).innerText = photographer.name;
             h2.classList.add("photographers__line--name");  
