2f86b0e98c1e348df0aab44e2d4d791e19575dea
Djo917 <trstenjak.jonathan@gmail.com>
2021-07-25 21:17:13 +0200

    création de la boucle for pour l'insertion des tags des photographes sous les portraits de ceux-ci

--- js/view.class.js
+++ js/view.class.js
@@ -2,7 +2,7 @@ export class View {
     renderAllPhotographers(photographers) {
         
         photographers.forEach(photographer => {
-            const idsection = document.getElementById("photographers");
+            const idSection = document.getElementById("photographers");
             const div = document.createElement("div");
             const a = document.createElement("a");
             const img = document.createElement("img");
@@ -10,30 +10,56 @@ export class View {
             const h3 = document.createElement("h3");
             const p = document.createElement("p");
             const pbis = document.createElement("p");  
+            const divTag = document.createElement("div");
+            
 
-            idsection.appendChild(div);
+            idSection.appendChild(div);
             div.classList.add("photographers__line--profile");
+
             div.appendChild(a);
             a.href = "photographer.html"
             a.classList.add("photographers__line--profilelink");
+
             a.appendChild(img);
             img.src = `media/sample_photos/Photographers_ID_Photos/${photographer.portrait}`;
             img.classList.add("photographers__line--pictures");
+
             a.appendChild(h2).innerText = photographer.name;
             h2.classList.add("photographers__line--name");  
+
             div.appendChild(h3).innerText = photographer.country;
             h3.classList.add("photographers__line--location", "margin--details");
+            
             div.appendChild(p).innerText = photographer.tagline;
             p.classList.add("photographers__line--skills", "margin--details");
             div.appendChild(pbis).innerText = photographer.price + '€/jour';
             pbis.classList.add("photographers__line--price", "margin--details");
+
+            div.appendChild(divTag);
+            divTag.classList.add("photographerstag");
+
             
+            
+            for (let i in photographer.tags) {
+                const linkTag = document.createElement("a");
+                const spanTag = document.createElement("span");
+
+                divTag.appendChild(linkTag);
+                linkTag.classList.add("photographerstag__link");
+
+                linkTag.appendChild(spanTag);
+                spanTag.classList.add("photographerstag__link--tag");
+
+                spanTag.innerText = '#' + photographer.tags[i];
+
+            }   
         });
 
     }
 
     renderAllTags() {
-        console.log('showTags');
+        console.log("showTags");
+        // linkTag.innerText = photographer[i].tags;
     }
 
 }
